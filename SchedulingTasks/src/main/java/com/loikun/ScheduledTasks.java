1ac95e4ba8fdcc74209288942becd8ac49a0c01c
polenger <polenger@qq.com>
2018-10-15 23:00:45 +0800

    2、Scheduling Task

--- SchedulingTasks/src/main/java/com/loikun/ScheduledTasks.java
+++ SchedulingTasks/src/main/java/com/loikun/ScheduledTasks.java
@@ -8,13 +8,19 @@ import org.springframework.stereotype.Component;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-
+/**
+ * 定时任务例子
+ */
 @Component
 public class ScheduledTasks {
+
     private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
+
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
+    // 定义每5秒执行任务，单位为毫秒
     @Scheduled(fixedRate = 5000)
+    //@Scheduled(cron = "")
     public void reportCurrentTime(){
         log.info("The time is now {}", dateFormat.format(new Date()));
     }
